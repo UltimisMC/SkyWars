384b119c951aeca006bce85592a7de240b69fa7f
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-06 00:16:41 +0100

    Added prestige level reward

---
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/level/PrestigeLevelReward.java
@@ -0,0 +1,23 @@
+package com.ultimismc.skywars.core.game.features.level;
+
+import com.ultimismc.skywars.core.user.User;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author DirectPlan
+ */
+@RequiredArgsConstructor
+public class PrestigeLevelReward implements LevelReward {
+
+    private final Prestige prestige;
+
+    @Override
+    public String getDisplayName() {
+        return prestige.getDisplayName();
+    }
+
+    @Override
+    public void rewardPlayer(User user) {
+
+    }
+}
