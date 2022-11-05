808927ef47f471c2411450a8ae6b733bc4df90d9
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-06 00:17:25 +0100

    Fixed typo in perk

--- core/src/main/java/com/ultimismc/skywars/core/game/features/perks/impl/MarksmanshipPerk.java
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/perks/impl/MarksmanshipPerk.java
@@ -11,6 +11,6 @@ public class MarksmanshipPerk extends AbstractPerk {
 
     public MarksmanshipPerk() {
         super(Material.BOW, "Marksmanship", PerkRarity.LEGENDARY,
-                "After getting 2 kills with a bow, all of your bows get enchanted with Power I");
+                "After getting 2 kills with a bow, all of your bows will get enchanted with Power I");
     }
 }
