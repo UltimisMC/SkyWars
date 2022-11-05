6fd317b7245327af0cd7a2db64225015f4efa2a3
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-06 00:14:27 +0100

    Added level class

--- core/src/main/java/com/ultimismc/skywars/core/game/features/level/Level.java
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/level/Level.java
@@ -1,9 +1,46 @@
 package com.ultimismc.skywars.core.game.features.level;
 
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author DirectPlan
  */
-public interface Level {
+@Data
+public class Level {
 
-    String getDisplayName();
+    private final int order, requiredExp;
+    private final Prestige prestige;
+
+    private final List<LevelReward> rewards = new ArrayList<>();
+
+    public void addReward(LevelReward levelReward) {
+        rewards.add(levelReward);
+    }
+
+    public void addAllRewards(List<LevelReward> levelRewards) {
+        rewards.addAll(levelRewards);
+    }
+    public void addAllRewards(LevelReward... levelRewards) {
+        addAllRewards(Arrays.asList(levelRewards));
+    }
+
+    public String getDisplayName(Prestige icon, boolean brackets) {
+        return prestige.getIconLevelDisplay(order, icon, brackets);
+    }
+
+    public String getDisplayName(Prestige prestigeIcon) {
+        return getDisplayName(prestigeIcon, false);
+    }
+
+    public String getDisplayName() {
+        return getDisplayName(null);
+    }
+
+    public boolean hasRewards() {
+        return !rewards.isEmpty();
+    }
 }
