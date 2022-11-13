6952172a55ec9a04ea1dccee78e5313475676fb3
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-13 19:12:30 +0100

    Added config keys for map.yml file

---
+++ game/src/main/java/com/ultimismc/skywars/game/config/MapConfigKeys.java
@@ -0,0 +1,34 @@
+package com.ultimismc.skywars.game.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import xyz.directplan.directlib.config.ConfigEntry;
+import xyz.directplan.directlib.config.replacement.ReplacementBoundary;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author DirectPlan
+ */
+@AllArgsConstructor
+@Getter
+public enum MapConfigKeys implements ConfigEntry {
+
+    MAP_NAME("name", "Ehh"),
+    MAP_SERIALIZED_ISLANDS("serialized-islands", Arrays.asList("")),
+    MAP_SERIALIZED_CHESTS("serialized-chests", Arrays.asList("")),
+    ;
+
+    private final String key;
+    @Setter
+    private Object value;
+    private boolean forceEntryDeclaration;
+    private final Map<String, ReplacementBoundary> replacementBoundaries = new HashMap<>();
+
+    MapConfigKeys(String key, Object value) {
+        this(key, value, true);
+    }
+}
