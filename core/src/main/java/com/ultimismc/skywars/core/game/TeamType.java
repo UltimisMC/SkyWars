9e9493ddaf6fa68c55f9cb26400f96272ed54c4b
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-13 19:14:58 +0100

    Each team type will have a maximum players for game

--- core/src/main/java/com/ultimismc/skywars/core/game/TeamType.java
+++ core/src/main/java/com/ultimismc/skywars/core/game/TeamType.java
@@ -9,15 +9,16 @@ import lombok.Getter;
 public enum TeamType {
 
 
-    SOLO("Solo", 1),
-    DOUBLES("Doubles",2),
+    SOLO("Solo", 12, 1),
+    DOUBLES("Doubles", 24,2),
     ;
 
     private final String name;
-    private final int maximumTeam;
+    private final int maximumPlayers, maximumTeam;
 
-    TeamType(String name, int maximumTeam) {
+    TeamType(String name, int maximumPlayers, int maximumTeam) {
         this.name = name;
+        this.maximumPlayers = maximumPlayers;
         this.maximumTeam = maximumTeam;
     }
 
