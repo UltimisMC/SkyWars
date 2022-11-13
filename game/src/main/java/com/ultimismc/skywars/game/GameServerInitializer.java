36bf706ebc67ecacf10aeebe859b59e85aef8ab5
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-13 19:11:13 +0100

    game server initializer

---
+++ game/src/main/java/com/ultimismc/skywars/game/GameServerInitializer.java
@@ -0,0 +1,114 @@
+package com.ultimismc.skywars.game;
+
+import com.ultimismc.skywars.core.SkyWarsPlugin;
+import com.ultimismc.skywars.core.game.GameServer;
+import com.ultimismc.skywars.core.game.GameType;
+import com.ultimismc.skywars.core.game.TeamType;
+import com.ultimismc.skywars.core.game.map.Island;
+import com.ultimismc.skywars.core.game.map.Map;
+import com.ultimismc.skywars.game.config.GameConfigKeys;
+import com.ultimismc.skywars.game.config.MapConfigKeys;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.Location;
+import xyz.directplan.directlib.CustomLocation;
+
+import java.util.List;
+
+/**
+ * @author DirectPlan
+ */
+@RequiredArgsConstructor
+public class GameServerInitializer {
+
+//    private final GameManager gameManager;
+
+    @Getter private GameServer gameServer;
+
+    public void initializeServer() {
+
+        GameInfo gameInfo = loadGameInfo();
+        GameType gameType = gameInfo.getGameType();
+        TeamType teamType = gameInfo.getTeamType();
+
+        String serverId = gameInfo.getServerId();
+
+        GameMap gameMap = loadGameMap();
+
+        gameServer = new GameServer(serverId, gameType, teamType, gameMap.toMap());
+
+        // Sending message to lobby about this server...
+
+    }
+
+    public void finalizeServer() {
+
+    }
+
+    private GameInfo loadGameInfo() {
+        String serverId = GameConfigKeys.SERVER_ID.getStringValue();
+        String gameTypeString = GameConfigKeys.GAME_TYPE.getStringValue();
+        String teamTypeString = GameConfigKeys.GAME_TEAM_TYPE.getStringValue();
+
+        GameType gameType = GameType.valueOf(gameTypeString);
+        TeamType teamType = TeamType.valueOf(teamTypeString);
+
+        return new GameInfo(serverId, gameType, teamType);
+    }
+
+    private GameMap loadGameMap() {
+
+        String name = MapConfigKeys.MAP_NAME.getStringValue();
+        List<String> serializedIslands = MapConfigKeys.MAP_SERIALIZED_ISLANDS.getStringList();
+//        List<String> serializedChests = MapConfigKeys.MAP_SERIALIZED_CHESTS.getStringList();
+
+        GameMap gameMap = new GameMap(name);
+
+
+        for(String serializedIsland : serializedIslands) {
+
+            String[] args = serializedIsland.split("/");
+            String serializedLocation = args[0];
+            gameMap.addIsland(serializedLocation);
+        }
+        return gameMap;
+    }
+
+    @Data
+    private static class GameInfo {
+
+        private final String id;
+        private final GameType gameType;
+        private final TeamType teamType;
+
+        public String getServerId() {
+            char gameChar = gameType.name().charAt(0);
+            char teamChar = teamType.name().charAt(0);
+            return id + teamChar + gameChar;
+        }
+    }
+
+    private static class GameMap {
+
+        private final Map map;
+
+        public GameMap(String name) {
+            map = new Map(name);
+        }
+
+        public void addIsland(Island island) {
+            map.addIsland(island);
+        }
+
+        public void addIsland(String serializedCageLocation) {
+            CustomLocation customLocation = CustomLocation.stringToLocation(serializedCageLocation);
+            Location cageLocation = customLocation.toBukkitLocation();
+            addIsland(new Island(cageLocation));
+        }
+
+        public Map toMap() {
+            return map;
+        }
+    }
+}
