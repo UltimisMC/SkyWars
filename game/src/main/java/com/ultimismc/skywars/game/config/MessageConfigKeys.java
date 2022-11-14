8a60bf85e43ca91ac6a0e4c1be31c1ee64d22939
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-14 23:47:14 +0100

    Added join & quit message Added scoreboard lines for Waiting & Started state

--- game/src/main/java/com/ultimismc/skywars/game/config/MessageConfigKeys.java
+++ game/src/main/java/com/ultimismc/skywars/game/config/MessageConfigKeys.java
@@ -17,14 +17,43 @@ import java.util.Map;
 @Getter
 public enum MessageConfigKeys implements ConfigEntry {
 
-    SKYWARS_LOBBY_SCOREBOARD_DISPLAYNAME("skywars-game.scoreboard.display-name", "&b&lSKYWARS"),
-    SKYWARS_LOBBY_SCOREBOARD_LINES("skywars-game.scoreboard.lines",
-            Arrays.asList("&7%servertime_MM/dd/yyyy% &8L21G",
+    SKYWARS_GAME_SCOREBOARD_DISPLAYNAME("skywars-game.scoreboard.display-name", "&b&lSKYWARS"),
+    SKYWARS_GAME_WAITING_SCOREBOARD_LINES("skywars-game.scoreboard-waiting.lines",
+            Arrays.asList("&7%servertime_MM/dd/yyyy% &8%server-id%",
                     " ",
-                    ""))
+                    "&fPlayers: &a%current-players/%maximum-players",
+                    " ",
+                    "%game-status%",
+                    " ",
+                    "&fMap: &a%map%",
+                    "&fMode: &a%mode%",
+                    " ",
+                    "&3play.ultimismc.com"
+                    )),
+    SKYWARS_GAME_STARTED_SCOREBOARD_LINES("skywars-game.scoreboard-started.lines",
+            Arrays.asList("&7%servertime_MM/dd/yyyy% &8%server-id%",
+                    " ",
+                    "&fNext Event:",
+                    "&a%next-event%",
+                    " ",
+                    "&fPlayers left: &a%players-left%",
+                    " ",
+                    "&fMap: %map%",
+                    "&fMode: %mode%",
+                    " ",
+                    "&3play.ultimismc.com"
+                    ))
 
     ,
+
+    JOIN_MESSAGE("join-message", "%player% &ehas joined (&b%current-players%&e/&b%maximum-players%&e)"),
+    QUIT_MESSAGE("quit-message", "%player% &ehas quit!"),
+
+    GAME_STARTS_IN_MESSAGE("game-starts-in-message", "&eThe game starts in %time%&e!"),
+
     ;
+
+
 
 
     private final String key;
