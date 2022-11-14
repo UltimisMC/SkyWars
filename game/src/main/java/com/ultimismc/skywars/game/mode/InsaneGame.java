7aad230e075a5fdfdf6cfcbae40b91c192ebe8d6
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-14 23:46:15 +0100

    Insane Game mode implementation

---
+++ game/src/main/java/com/ultimismc/skywars/game/mode/InsaneGame.java
@@ -0,0 +1,19 @@
+package com.ultimismc.skywars.game.mode;
+
+import com.ultimismc.skywars.core.user.User;
+
+/**
+ * @author DirectPlan
+ */
+public class InsaneGame extends AbstractGame {
+
+    public InsaneGame(GameHandler gameHandler) {
+        super(gameHandler);
+    }
+
+    @Override
+    public void prepareUser(User user) {
+        super.prepareUser(user);
+
+    }
+}
