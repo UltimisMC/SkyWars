490b3c98f97e1edcb243d0c2f5baf844b7cadf82
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-14 23:46:22 +0100

    Normal Game mode implementation

---
+++ game/src/main/java/com/ultimismc/skywars/game/mode/NormalGame.java
@@ -0,0 +1,19 @@
+package com.ultimismc.skywars.game.mode;
+
+import com.ultimismc.skywars.core.user.User;
+
+/**
+ * @author DirectPlan
+ */
+public class NormalGame extends AbstractGame {
+
+    public NormalGame(GameHandler gameHandler) {
+        super(gameHandler);
+    }
+
+    @Override
+    public void prepareUser(User user) {
+        super.prepareUser(user);
+
+    }
+}
