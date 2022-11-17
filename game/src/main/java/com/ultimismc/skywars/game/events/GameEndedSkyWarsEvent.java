d3703d5be8bd3525be09f61cb114e1544a5ad427
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-17 20:03:38 +0100

    Added game ended skywars event

---
+++ game/src/main/java/com/ultimismc/skywars/game/events/GameEndedSkyWarsEvent.java
@@ -0,0 +1,19 @@
+package com.ultimismc.skywars.game.events;
+
+import com.ultimismc.skywars.game.handler.Game;
+import com.ultimismc.skywars.game.handler.GameHandler;
+
+/**
+ * @author DirectPlan
+ */
+public class GameEndedSkyWarsEvent extends AbstractSkyWarsEvent {
+
+    public GameEndedSkyWarsEvent(long scheduledIn) {
+        super("Game Ended!", scheduledIn);
+    }
+
+    @Override
+    public void executeEvent(GameHandler gameHandler) {
+
+    }
+}
