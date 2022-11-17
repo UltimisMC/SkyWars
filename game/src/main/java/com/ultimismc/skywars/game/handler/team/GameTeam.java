55225b426c096de55141f578fba018a11f5af9e9
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-17 20:12:24 +0100

    Added empty game team class

---
+++ game/src/main/java/com/ultimismc/skywars/game/handler/team/GameTeam.java
@@ -0,0 +1,12 @@
+package com.ultimismc.skywars.game.handler.team;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author DirectPlan
+ */
+@RequiredArgsConstructor
+public class GameTeam {
+
+    private final char gameChar;
+}
