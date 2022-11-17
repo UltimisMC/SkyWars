79b78252e1c53ae85fb3e28a2182c7d88643a138
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-17 20:05:33 +0100

    Players will not be able to join if the game has started or full

--- game/src/main/java/com/ultimismc/skywars/game/GameListener.java
+++ game/src/main/java/com/ultimismc/skywars/game/GameListener.java
@@ -1,11 +1,25 @@
 package com.ultimismc.skywars.game;
 
+import com.ultimismc.skywars.game.handler.GameHandler;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import xyz.directplan.directlib.PluginUtility;
 
 /**
  * @author DirectPlan
  */
+@RequiredArgsConstructor
 public class GameListener implements Listener {
 
+    private final GameHandler gameHandler;
 
+    @EventHandler
+    public void onPreJoin(AsyncPlayerPreLoginEvent event) {
+        if(gameHandler.isOpen()) return;
+
+        event.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_WHITELIST);
+        event.setKickMessage(PluginUtility.translateMessage("&cGame is full or has already started!"));
+    }
 }
