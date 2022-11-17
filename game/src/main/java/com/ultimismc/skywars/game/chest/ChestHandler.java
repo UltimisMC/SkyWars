7ffafec448d870d8967d9a5fba6a8d2f82bbe6f5
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-17 15:25:45 +0100

    A Chest handler. Responsible for refills, holograms, etc...

---
+++ game/src/main/java/com/ultimismc/skywars/game/chest/ChestHandler.java
@@ -0,0 +1,16 @@
+package com.ultimismc.skywars.game.chest;
+
+import com.ultimismc.skywars.game.handler.GameHandler;
+
+/**
+ * @author DirectPlan
+ */
+public class ChestHandler {
+
+    private final GameHandler gameHandler;
+
+    public ChestHandler(GameHandler gameHandler) {
+        this.gameHandler = gameHandler;
+    }
+
+}
