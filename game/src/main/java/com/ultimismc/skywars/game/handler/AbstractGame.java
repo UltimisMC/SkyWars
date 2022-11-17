b115cdc5bfd92a9c889ff77c8b06de80de3fcd1e
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-17 15:24:56 +0100

    Added game methods

--- game/src/main/java/com/ultimismc/skywars/game/handler/AbstractGame.java
+++ game/src/main/java/com/ultimismc/skywars/game/handler/AbstractGame.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author DirectPlan
  */
@@ -15,20 +18,54 @@ public abstract class AbstractGame implements Game {
 
     private final GameHandler gameHandler;
 
+    private final int minimumPlayers = 8;
     private GameState gameState = GameState.WAITING;
+    private final List<User> players = new ArrayList<>();
+    private final List<User> spectators = new ArrayList<>();
+
+    @Override
+    public void startGame() {
+
+    }
+
+    @Override
+    public void endGame() {
+
+    }
 
     @Override
     public void prepareUser(User user) {
-
+        players.add(user);
     }
 
     @Override
     public void quitUser(User user) {
-
+        players.remove(user);
+        removeSpectator(user);
     }
 
     @Override
-    public int getPlayersLeft() {
-        return 0;
+    public void addSpectator(User user) {
+        spectators.add(user);
+    }
+
+    @Override
+    public void removeSpectator(User user) {
+        spectators.remove(user);
+    }
+
+    @Override
+    public boolean isWaiting() {
+        return gameState == GameState.WAITING;
+    }
+
+    @Override
+    public boolean isStarting() {
+        return gameState == GameState.STARTING;
+    }
+
+    @Override
+    public boolean hasStarted() {
+        return gameState == GameState.STARTED;
     }
 }
