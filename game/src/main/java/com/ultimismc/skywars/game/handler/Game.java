4aa57ef2058988763ad2ad9df80014fb3d0f5881
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-17 15:25:55 +0100

    Added general game methods

--- game/src/main/java/com/ultimismc/skywars/game/handler/Game.java
+++ game/src/main/java/com/ultimismc/skywars/game/handler/Game.java
@@ -2,18 +2,38 @@ package com.ultimismc.skywars.game.handler;
 
 import com.ultimismc.skywars.core.user.User;
 
+import java.util.List;
+
 /**
  * @author DirectPlan
  */
 public interface Game {
 
-    GameState getGameState();
+    void startGame();
 
-    void setGameState(GameState gameState);
-
-    int getPlayersLeft();
+    void endGame();
 
     void prepareUser(User user);
 
     void quitUser(User user);
+
+    GameState getGameState();
+
+    void setGameState(GameState gameState);
+
+    List<User> getSpectators();
+
+    List<User> getPlayers();
+
+    void addSpectator(User user);
+
+    void removeSpectator(User user);
+
+    boolean isWaiting();
+
+    boolean isStarting();
+
+    boolean hasStarted();
+
+    int getMinimumPlayers();
 }
