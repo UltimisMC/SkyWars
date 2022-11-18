d9369be0c72f87d5fbdd61b5b964827d114545d1
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-18 01:35:53 +0100

    Implemented refill skywars game event

--- game/src/main/java/com/ultimismc/skywars/game/events/RefillSkyWarsEvent.java
+++ game/src/main/java/com/ultimismc/skywars/game/events/RefillSkyWarsEvent.java
@@ -1,6 +1,10 @@
 package com.ultimismc.skywars.game.events;
 
+import com.ultimismc.skywars.game.chest.ChestHandler;
 import com.ultimismc.skywars.game.handler.GameHandler;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import xyz.directplan.directlib.PluginUtility;
 
 /**
  * @author DirectPlan
@@ -13,6 +17,12 @@ public class RefillSkyWarsEvent extends AbstractSkyWarsEvent {
 
     @Override
     public void executeEvent(GameHandler gameHandler) {
-
+        ChestHandler chestHandler = gameHandler.getChestHandler();
+        chestHandler.refillAllChests();
+        gameHandler.broadcastFunction(user -> {
+            Player player = user.getPlayer();
+            PluginUtility.playSound(player, Sound.CHEST_OPEN);
+            PluginUtility.sendTitle(player, "&eAll chests have been refilled!");
+        });
     }
 }
