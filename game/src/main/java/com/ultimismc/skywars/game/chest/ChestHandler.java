fc2e314a029494536999fc5e27d1d548080b04f6
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-18 01:32:29 +0100

    Added chest handler methods

--- game/src/main/java/com/ultimismc/skywars/game/chest/ChestHandler.java
+++ game/src/main/java/com/ultimismc/skywars/game/chest/ChestHandler.java
@@ -1,6 +1,12 @@
 package com.ultimismc.skywars.game.chest;
 
 import com.ultimismc.skywars.game.handler.GameHandler;
+import lombok.Getter;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author DirectPlan
@@ -8,9 +14,50 @@ import com.ultimismc.skywars.game.handler.GameHandler;
 public class ChestHandler {
 
     private final GameHandler gameHandler;
+    @Getter private final Map<Location, Chest> chests = new HashMap<>();
+
 
     public ChestHandler(GameHandler gameHandler) {
         this.gameHandler = gameHandler;
     }
 
+    public void refillChest(Chest chest) {
+
+        boolean midChest = chest.isMidChest();
+
+    }
+
+    public void refillAllChests() {
+        chests.forEach((location, chest) -> refillChest(chest));
+    }
+
+    public void addChest(Chest chest) {
+        chests.put(chest.getLocation(), chest);
+    }
+
+    public void addChest(Block block, boolean midChest) {
+        addChest(new Chest(block, midChest));
+    }
+
+    public Chest getChest(Location location) {
+        return chests.get(location);
+    }
+
+    public Chest getChest(Block block) {
+        Location location = block.getLocation();
+        return getChest(location);
+    }
+
+    public void removeChest(Location location) {
+        chests.remove(location);
+    }
+
+    public void removeChest(Block block) {
+        Location location = block.getLocation();
+        removeChest(location);
+    }
+
+    public int getSize() {
+        return chests.size();
+    }
 }
