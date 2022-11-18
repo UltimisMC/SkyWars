d9a1024fe9f4636c0c7cb42c7259fbea711925f7
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-18 01:30:09 +0100

    Added cage classes

---
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/cosmetics/cages/Cage.java
@@ -0,0 +1,11 @@
+package com.ultimismc.skywars.core.game.features.cosmetics.cages;
+
+/**
+ * @author DirectPlan
+ */
+public interface Cage {
+
+    String getName();
+
+    CageSchematic getSchematic();
+}
---
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/cosmetics/cages/CageSchematic.java
@@ -0,0 +1,7 @@
+package com.ultimismc.skywars.core.game.features.cosmetics.cages;
+
+/**
+ * @author DirectPlan
+ */
+public class CageSchematic {
+}
