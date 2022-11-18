8ad0e0cf66077290f51a7799e7c7817087c10dd8
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-18 01:30:17 +0100

    Added spectator menu bar

---
+++ game/src/main/java/com/ultimismc/skywars/game/menubar/GameSpectatorBarMenu.java
@@ -0,0 +1,30 @@
+package com.ultimismc.skywars.game.menubar;
+
+import com.ultimismc.skywars.core.user.User;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import xyz.directplan.directlib.inventory.MenuItem;
+
+/**
+ * @author DirectPlan
+ */
+public class GameSpectatorBarMenu extends GameBarMenu {
+
+    public GameSpectatorBarMenu(User user) {
+        super(user);
+    }
+
+    @Override
+    public void build(Player player) {
+        super.build(player);
+
+        MenuItem teleporterItem = addBarItem(0, Material.COMPASS, "&a&lTeleporter");
+        teleporterItem.setLore("&7Right-click to spectate players!");
+
+        MenuItem spectatorSettingsItem = addBarItem(4, Material.REDSTONE_COMPARATOR, "&b&lSpectator Settings");
+        spectatorSettingsItem.setLore("&7Right-click to change your spectator settings!");
+
+        MenuItem playAgainItem = addBarItem(7, Material.PAPER, "&b&lPlay Again");
+        playAgainItem.setLore("&7Righ-click to play another game!");
+    }
+}
