cdd0bc4744f41029e8598b925f3f976dedfa8cd4
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-20 02:19:58 +0100

    cosmetic manager

---
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/cosmetics/CosmeticManager.java
@@ -0,0 +1,36 @@
+package com.ultimismc.skywars.core.game.features.cosmetics;
+
+import com.ultimismc.skywars.core.SkyWarsPlugin;
+import com.ultimismc.skywars.core.game.features.FeatureHandler;
+import com.ultimismc.skywars.core.game.features.FeatureInitializer;
+import com.ultimismc.skywars.core.game.features.PurchasableFeature;
+import com.ultimismc.skywars.core.game.features.cosmetics.cages.CageHandler;
+import lombok.Getter;
+
+/**
+ * @author DirectPlan
+ */
+public class CosmeticManager implements FeatureInitializer {
+
+    @Getter private final String name = "Cosmetics";
+
+    private final FeatureHandler featureHandler;
+    private final CageHandler cageHandler;
+
+    public CosmeticManager(FeatureHandler featureHandler, SkyWarsPlugin plugin) {
+        this.featureHandler = featureHandler;
+        cageHandler = new CageHandler(plugin);
+
+        registerCosmeticHandler(cageHandler);
+    }
+
+    @Override
+    public void initializeFeature(SkyWarsPlugin plugin) {
+
+
+    }
+
+    public <T extends PurchasableFeature<?>> void registerCosmeticHandler(T cosmeticHandler) {
+        featureHandler.addInitializers(cosmeticHandler);
+    }
+}
