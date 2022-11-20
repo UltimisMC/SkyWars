16879499934dfca01f2c551abf87aa312cff85e8
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-20 02:14:21 +0100

    Implemented Cage class

--- core/src/main/java/com/ultimismc/skywars/core/game/features/cosmetics/cages/Cage.java
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/cosmetics/cages/Cage.java
@@ -1,11 +1,28 @@
 package com.ultimismc.skywars.core.game.features.cosmetics.cages;
 
+import com.ultimismc.skywars.core.game.features.PurchasableDesign;
+import com.ultimismc.skywars.core.game.features.cosmetics.AbstractCosmetic;
+import com.ultimismc.skywars.core.game.features.cosmetics.CosmeticRarity;
+import com.ultimismc.skywars.core.game.features.cosmetics.cages.schematic.CageSchematic;
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.Location;
+
 /**
  * @author DirectPlan
  */
-public interface Cage {
+@Getter
+public class Cage extends AbstractCosmetic {
 
-    String getName();
+    @Setter private CageSchematic schematic;
 
-    CageSchematic getSchematic();
+    public Cage(CageSchematic schematic, PurchasableDesign design, String name, CosmeticRarity rarity) {
+        super(design.getMaterial(), design.getDurability(), name, rarity);
+
+        this.schematic = schematic;
+    }
+
+    public void placeSchematic(Location location) {
+        schematic.placeSchematic(location);
+    }
 }
