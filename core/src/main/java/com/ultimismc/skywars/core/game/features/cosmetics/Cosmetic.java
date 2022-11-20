ec8bd79388a3467b911570ae754641510e6a1ae7
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-20 02:20:21 +0100

    Cosmetic interface

---
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/cosmetics/Cosmetic.java
@@ -0,0 +1,11 @@
+package com.ultimismc.skywars.core.game.features.cosmetics;
+
+import com.ultimismc.skywars.core.game.features.Purchasable;
+
+/**
+ * @author DirectPlan
+ */
+public interface Cosmetic extends Purchasable {
+
+
+}
