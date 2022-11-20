d7bb374da3e93b55c6af5d5d5e36eabbb7089974
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-20 02:20:13 +0100

    CageCosmetic interface

---
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/cosmetics/cages/schematic/CageSchematic.java
@@ -0,0 +1,17 @@
+package com.ultimismc.skywars.core.game.features.cosmetics.cages.schematic;
+
+import org.bukkit.Location;
+
+import java.io.File;
+
+/**
+ * @author DirectPlan
+ */
+public interface CageSchematic {
+
+    File getSchematicFile();
+
+    void placeSchematic(Location location);
+
+    void saveSchematic(File file);
+}
