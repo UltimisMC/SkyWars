980863f0c0fcad52d8f61b3f5a90c0013d315ba0
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-21 00:54:34 +0100

    Re designed user settings handler

--- core/src/main/java/com/ultimismc/skywars/core/user/setting/UserSettingHandler.java
+++ core/src/main/java/com/ultimismc/skywars/core/user/setting/UserSettingHandler.java
@@ -2,33 +2,53 @@ package com.ultimismc.skywars.core.user.setting;
 
 import com.ultimismc.skywars.core.user.UserCacheHandler;
 
-import java.util.Collection;
+import java.util.*;
 
 /**
  * @author DirectPlan
  */
 public class UserSettingHandler extends UserCacheHandler<String, UserSetting> {
 
-    public <T> void setSetting(String name, T value) {
-        UserSetting userSetting = userCache.computeIfAbsent(name, s -> new UserSetting(name, value));
-        userSetting.setValue(value);
+    public <T> void addSetting(String key, T value) {
+        UserSetting userSetting = userCache.get(key);
+        if(userSetting != null) {
+            Object settingValue = userSetting.getValue();
+            List<T> values;
+            if(!(settingValue instanceof List)) {
+                values = new ArrayList<>();
+            }else {
+                values = (List<T>) settingValue;
+            }
+            values.add(value);
+            return;
+        }
+        addCache(key, new UserSetting(key, value));
     }
 
-    public boolean isActivated(String name) {
-        UserSetting userSetting = getCache(name);
-        return userSetting.asBoolean();
+    public <T> T getSetting(Class<T> castClass, String key) {
+        UserSetting userSetting = userCache.get(key);
+        if(userSetting != null) {
+            Object value = userSetting.getValue();
+            return castClass.cast(value);
+        }
+        return null;
     }
 
-    public int getSettingInt(String name) {
-        UserSetting userSetting = getCache(name);
-        return userSetting.asInt();
+    public boolean isEmpty() {
+        return userCache.isEmpty();
     }
 
-    public String getSettingValue(String name) {
-        UserSetting userSetting = getCache(name);
-        return userSetting.asString();
+    public boolean isActivated(String key) {
+        return getSetting(Boolean.class, key);
     }
 
+    public int getSettingInt(SettingCategory category, String key) {
+        return getSetting(Integer.class, key);
+    }
+
+    public String getSettingString(SettingCategory category, String key) {
+        return getSetting(String.class, key);
+    }
 
     public Collection<UserSetting> getSettings() {
         return getCacheCollection();
