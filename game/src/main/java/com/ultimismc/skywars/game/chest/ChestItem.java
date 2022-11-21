09d48c0a56af59a7d83e682865cd30f45a122870
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-22 00:53:19 +0100

    Implemented ChestItem class

--- game/src/main/java/com/ultimismc/skywars/game/chest/ChestItem.java
+++ game/src/main/java/com/ultimismc/skywars/game/chest/ChestItem.java
@@ -1,7 +1,40 @@
 package com.ultimismc.skywars.game.chest;
 
+import lombok.Getter;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import xyz.directplan.directlib.inventory.ItemEnchantment;
+import xyz.directplan.directlib.inventory.MenuItem;
+
 /**
  * @author DirectPlan
  */
+@Getter
 public class ChestItem {
+
+    private final int percentage;
+    private final MenuItem menuItem;
+
+    public ChestItem(int percentage, Material material, int amount) {
+        this.percentage = percentage;
+        menuItem = new MenuItem(material, null);
+        menuItem.setAmount(amount);
+    }
+
+    public ChestItem(int percentage, Material material) {
+        this(percentage, material, 1);
+    }
+
+    public void addEnchantment(ItemEnchantment enchantment) {
+        menuItem.addEnchantments(enchantment);
+    }
+
+
+    public int getItemAmount() {
+        return menuItem.getItemAmount();
+    }
+
+    public ItemStack getItemStack() {
+        return menuItem.getItemStack();
+    }
 }
