8ce9536145dedb89e07c28c90fdd9447abb5606f
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-22 00:54:31 +0100

    Added mode display name

--- core/src/main/java/com/ultimismc/skywars/core/game/GameType.java
+++ core/src/main/java/com/ultimismc/skywars/core/game/GameType.java
@@ -1,20 +1,27 @@
 package com.ultimismc.skywars.core.game;
 
 import lombok.Getter;
+import org.bukkit.ChatColor;
 
 /**
  * @author DirectPlan
  */
+@Getter
 public enum GameType {
 
-    NORMAL("Normal"),
-    INSANE("Insane"),
+    NORMAL(ChatColor.GREEN, "Normal"),
+    INSANE(ChatColor.RED, "Insane"),
     ;
 
-    @Getter private final String name;
+    private final ChatColor color;
+    private final String name;
 
-    GameType(String name) {
+    GameType(ChatColor color, String name) {
         this.name = name;
+        this.color = color;
     }
 
+    public String getDisplayName() {
+        return this.color + this.name;
+    }
 }
