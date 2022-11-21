0aadcc3543059b9129f9188ba8e59f3d33a680f8
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-22 00:55:06 +0100

    Insane mode will have a title showing once the game starts

--- game/src/main/java/com/ultimismc/skywars/game/mode/InsaneGame.java
+++ game/src/main/java/com/ultimismc/skywars/game/mode/InsaneGame.java
@@ -3,6 +3,8 @@ package com.ultimismc.skywars.game.mode;
 import com.ultimismc.skywars.core.user.User;
 import com.ultimismc.skywars.game.handler.AbstractGame;
 import com.ultimismc.skywars.game.handler.GameHandler;
+import org.bukkit.entity.Player;
+import xyz.directplan.directlib.PluginUtility;
 
 /**
  * @author DirectPlan
@@ -17,5 +19,15 @@ public class InsaneGame extends AbstractGame {
     public void prepareUser(User user) {
         super.prepareUser(user);
 
+    }
+
+    @Override
+    public void startGame() {
+        super.startGame();
+
+        gameHandler.broadcastFunction(user -> {
+            Player player = user.getPlayer();
+            PluginUtility.sendTitle(player, 20, 40, 20, "&c&lINSANE MODE");
+        });
     }
 }
