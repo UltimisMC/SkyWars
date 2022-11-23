095706969b512ea72cebef92ef8d72ef9c68260c
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-23 21:44:57 +0100

    Added a very important note

--- core/src/main/java/com/ultimismc/skywars/core/user/UserSavedListener.java
+++ core/src/main/java/com/ultimismc/skywars/core/user/UserSavedListener.java
@@ -2,6 +2,9 @@ package com.ultimismc.skywars.core.user;
 
 /**
  * @author DirectPlan
+ *
+ * Make sure that when the user gets saved
+ * the default purchasables don't get saved too.
  */
 public class UserSavedListener {
 
