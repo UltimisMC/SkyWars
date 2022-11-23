6aee39958e763404c715287854c1b84b73b71be7
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-23 21:42:57 +0100

    Removed redundancy

--- lobby/src/main/java/com/ultimismc/skywars/lobby/shop/kitsandperks/kits/KitProduct.java
+++ lobby/src/main/java/com/ultimismc/skywars/lobby/shop/kitsandperks/kits/KitProduct.java
@@ -2,13 +2,9 @@ package com.ultimismc.skywars.lobby.shop.kitsandperks.kits;
 
 import com.ultimismc.skywars.core.game.features.PurchasableRarity;
 import com.ultimismc.skywars.core.game.features.kits.Kit;
-import com.ultimismc.skywars.core.game.features.kits.KitItem;
-import com.ultimismc.skywars.core.game.features.kits.KitRarity;
 import com.ultimismc.skywars.core.user.User;
 import com.ultimismc.skywars.lobby.shop.UserPurchasableProduct;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import xyz.directplan.directlib.inventory.ItemEnchantment;
 import xyz.directplan.directlib.shop.ProductItemDesign;
 
 import java.util.ArrayList;
@@ -28,17 +24,9 @@ public class KitProduct extends UserPurchasableProduct {
 
     @Override
     public ProductItemDesign designPurchasableProduct(User user) {
-        List<String> lore = new ArrayList<>();
+        List<String> lore = new ArrayList<>(kit.getDisplayItems()); // <- Kit display items
 
         PurchasableRarity rarity = kit.getRarity();
-        List<KitItem> kitItems = kit.getItems();
-        for(KitItem kitItem : kitItems) {
-
-            lore.add(ChatColor.GRAY + kitItem.getDisplayName());
-            for(ItemEnchantment enchantment : kitItem.getEnchantments()) {
-                lore.add("   * " + enchantment.getDisplayName());
-            }
-        }
         lore.add(" ");
         lore.add("&7Rarity: " + rarity.getDisplayName());
 
@@ -52,5 +40,6 @@ public class KitProduct extends UserPurchasableProduct {
     public void executePurchasableProduct(User user) {
         Player player = user.getPlayer();
         user.sendMessage("&aPreviewing &e" + kit.getName() + " &akit...");
+
     }
 }
