f95cea073a37779d948d8fe46ad92417084f947d
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-23 21:42:10 +0100

    Added selected kit action bar

--- game/src/main/java/com/ultimismc/skywars/game/handler/runnable/GameRunnable.java
+++ game/src/main/java/com/ultimismc/skywars/game/handler/runnable/GameRunnable.java
@@ -1,7 +1,10 @@
 package com.ultimismc.skywars.game.handler.runnable;
 
+import com.ultimismc.skywars.core.game.features.kits.Kit;
 import com.ultimismc.skywars.game.handler.GameHandler;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.entity.Player;
+import xyz.directplan.directlib.PluginUtility;
 
 /**
  * @author DirectPlan
@@ -13,8 +16,17 @@ public class GameRunnable implements Runnable {
 
     @Override
     public void run() {
+
+        if(!gameHandler.hasStarted()) {
+            gameHandler.broadcastFunction(user -> {
+                Kit kit = user.getSetting(Kit.class, "kit");
+                PluginUtility.sendActionBar(user.getPlayer(), "&eSelected Kit: &a" + kit.getName());
+            });
+            return;
+        }
         long gameTime = gameHandler.getGameTime();
         gameHandler.setGameTime(gameTime += 1000L);
+
 
         gameHandler.updateScoreboard();
     }
