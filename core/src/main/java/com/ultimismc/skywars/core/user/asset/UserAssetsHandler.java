f7fb447644554a752ced03dec2c53b1f4c7f6a93
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-23 21:44:17 +0100

    Added getAssets via generic class

--- core/src/main/java/com/ultimismc/skywars/core/user/asset/UserAssetsHandler.java
+++ core/src/main/java/com/ultimismc/skywars/core/user/asset/UserAssetsHandler.java
@@ -3,7 +3,9 @@ package com.ultimismc.skywars.core.user.asset;
 import com.ultimismc.skywars.core.game.features.Purchasable;
 import com.ultimismc.skywars.core.user.UserCacheHandler;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * @author DirectPlan
@@ -29,6 +31,16 @@ public class UserAssetsHandler extends UserCacheHandler<String, UserAsset> {
 
     public boolean hasPurchased(Purchasable purchasable) {
         return getAsset(purchasable) != null;
+    }
+
+    public <T extends Purchasable> List<T> getAssets(Class<T> clazz) {
+        List<T> assets = new ArrayList<>();
+        for(UserAsset userAsset : getAssets()) {
+            Purchasable purchasable = userAsset.getPurchasable();
+            if(!clazz.isInstance(purchasable)) continue;
+            assets.add(clazz.cast(purchasable));
+        }
+        return assets;
     }
 
     public Collection<UserAsset> getAssets() {
