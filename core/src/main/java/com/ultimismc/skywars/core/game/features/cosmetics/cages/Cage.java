6e58f5c1428f51caa9ac3ce2d21fd225477fd92b
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-24 23:33:55 +0100

    Cage will now hold two schematics, solo and team.

--- core/src/main/java/com/ultimismc/skywars/core/game/features/cosmetics/cages/Cage.java
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/cosmetics/cages/Cage.java
@@ -1,5 +1,6 @@
 package com.ultimismc.skywars.core.game.features.cosmetics.cages;
 
+import com.ultimismc.skywars.core.game.TeamType;
 import com.ultimismc.skywars.core.game.features.PurchasableDesign;
 import com.ultimismc.skywars.core.game.features.cosmetics.AbstractCosmetic;
 import com.ultimismc.skywars.core.game.features.cosmetics.CosmeticRarity;
@@ -15,17 +16,19 @@ import org.bukkit.Location;
 
 public class Cage extends AbstractCosmetic {
 
-    @Setter private CageSchematic schematic;
+    @Setter private CageSchematic soloSchematic, teamSchematic;
     private final String category = "Cage";
     @Setter private boolean defaultCage = false;
 
-    public Cage(CageSchematic schematic, PurchasableDesign design, String name, CosmeticRarity rarity) {
+    public Cage(CageSchematic soloSchematic, CageSchematic teamSchematic, PurchasableDesign design, String name, CosmeticRarity rarity) {
         super(design.getMaterial(), design.getDurability(), name, rarity);
 
-        this.schematic = schematic;
+        this.soloSchematic = soloSchematic;
+        this.teamSchematic = teamSchematic;
     }
 
-    public void placeSchematic(Location location, boolean ignoreAir) {
+    public void placeSchematic(TeamType teamType, Location location, boolean ignoreAir) {
+        CageSchematic schematic = (teamType.isSolo() ? soloSchematic : teamSchematic);
         schematic.placeSchematic(location, ignoreAir);
     }
 
