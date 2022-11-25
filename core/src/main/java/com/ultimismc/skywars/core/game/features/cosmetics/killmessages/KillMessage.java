2fa972a076bd8cb30e9e29a8314d6580d61dca09
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-25 01:19:55 +0100

    Kill message

---
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/cosmetics/killmessages/KillMessage.java
@@ -0,0 +1,11 @@
+package com.ultimismc.skywars.core.game.features.cosmetics.killmessages;
+
+import com.ultimismc.skywars.core.game.features.Purchasable;
+
+/**
+ * @author DirectPlan
+ */
+public interface KillMessage extends Purchasable {
+
+
+}
