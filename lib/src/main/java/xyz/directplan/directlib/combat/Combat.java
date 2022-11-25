ce014d5cda5b3b2fc790ddfc82eeaa36738f09d0
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-25 01:18:56 +0100

    Added 5 seconds combat expiration

--- lib/src/main/java/xyz/directplan/directlib/combat/Combat.java
+++ lib/src/main/java/xyz/directplan/directlib/combat/Combat.java
@@ -15,7 +15,7 @@ public class Combat {
     private final long attackedAt;
 
     public Combat(Player player) {
-        this(player, System.currentTimeMillis());
+        this(player, System.currentTimeMillis() + 5000L);
     }
 
     public boolean hasExpired() {
