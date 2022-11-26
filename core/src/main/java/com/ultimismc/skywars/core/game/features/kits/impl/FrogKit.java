e7d6a748a98a74659e402f04a805175f87fb29ba
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-26 03:20:40 +0100

    Added frog head texture to frog kit

--- core/src/main/java/com/ultimismc/skywars/core/game/features/kits/impl/FrogKit.java
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/kits/impl/FrogKit.java
@@ -4,7 +4,6 @@ import com.ultimismc.skywars.core.game.features.PurchasableDesign;
 import com.ultimismc.skywars.core.game.features.kits.AbstractKit;
 import com.ultimismc.skywars.core.game.features.kits.KitRarity;
 import lombok.Getter;
-import org.bukkit.Material;
 
 /**
  * @author DirectPlan
@@ -12,7 +11,7 @@ import org.bukkit.Material;
 @Getter
 public class FrogKit extends AbstractKit {
 
-    private final PurchasableDesign design = new PurchasableDesign(Material.SLIME_BALL);
+    private final PurchasableDesign design = new PurchasableDesign("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYzE3ODE4OWVmOGZhN2E1YjcyNGZiOTFkZjlhNDQ3ODRmZDg1NjQ4ZWQzZTNhY2Y2ZDBkZWQ3YjhjYWEzMGYwNyJ9fX0=");
 
     public FrogKit() {
         super("Frog", KitRarity.COMMON);
