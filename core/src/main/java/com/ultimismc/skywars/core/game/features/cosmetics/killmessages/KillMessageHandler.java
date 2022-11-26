e8ca138c10c8567db1ef8e645e19a160296ababa
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-26 02:21:28 +0100

    Added kill message cosmetic handler Registered all kill message purchasables

--- core/src/main/java/com/ultimismc/skywars/core/game/features/cosmetics/killmessages/KillMessageHandler.java
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/cosmetics/killmessages/KillMessageHandler.java
@@ -2,28 +2,43 @@ package com.ultimismc.skywars.core.game.features.cosmetics.killmessages;
 
 import com.ultimismc.skywars.core.SkyWarsPlugin;
 import com.ultimismc.skywars.core.game.features.PurchasableFeature;
+import com.ultimismc.skywars.core.game.features.cosmetics.killmessages.impl.*;
 import lombok.Getter;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author DirectPlan
  */
 @Getter
-public class KillMessageHandler implements PurchasableFeature<KillMessage> {
+public class KillMessageHandler extends  PurchasableFeature<KillMessage> {
 
     private final String name = "Kill Messages";
 
-    private final Map<String, KillMessage> killMessages = new HashMap<>();
-
     @Override
     public void initializeFeature(SkyWarsPlugin plugin) {
-
-    }
-
-    @Override
-    public Map<String, KillMessage> getPurchasables() {
-        return killMessages;
+        registerPurchasable(new DefaultKillMessage()); // Done
+        registerPurchasable(new FireKillMessage()); // Done
+        registerPurchasable(new WesternKillMessage()); // Done
+        registerPurchasable(new LoveKillMessage()); // Done
+        registerPurchasable(new PirateKillMessage()); // Done
+        registerPurchasable(new GalacticKillMessage()); // Done
+        registerPurchasable(new ComputerKillMessage()); // Done
+        registerPurchasable(new HonourableKillMessage()); // Done
+        registerPurchasable(new FlexKillMessage()); // Done
+        registerPurchasable(new RainbowKillMessage()); // Done
+        registerPurchasable(new BBQKillMessage()); // Done
+        registerPurchasable(new InsectoidKillMessage()); // Done
+        registerPurchasable(new BananaKillMessage()); // Done
+        registerPurchasable(new SqueakKillMessage()); // Done
+        registerPurchasable(new OinkKillMessage()); // Done
+        registerPurchasable(new BuzzKillMessage()); // Done
+        registerPurchasable(new BoxingKillMessage()); // Done
+        registerPurchasable(new OxdKillMessage()); // Done
+        registerPurchasable(new MedievalKillMessage()); // Done
+        registerPurchasable(new WOOFKillMessage()); // Done
+        registerPurchasable(new MemedKillMessage()); // Done
+        registerPurchasable(new SnowStormKillMessage()); // Done
+        registerPurchasable(new EggyKillMessage()); // Done
+        registerPurchasable(new ToTheMoonKillMessage()); // Done
+        registerPurchasable(new RoarKillMessage()); // Done
     }
 }
