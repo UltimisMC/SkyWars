fdefe72f279f2e5038d0f7f9cb2b3ba635b1c0da
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-26 02:19:43 +0100

    Improved purchasable design

--- core/src/main/java/com/ultimismc/skywars/core/game/features/kits/impl/EngineerKit.java
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/kits/impl/EngineerKit.java
@@ -1,15 +1,20 @@
 package com.ultimismc.skywars.core.game.features.kits.impl;
 
+import com.ultimismc.skywars.core.game.features.PurchasableDesign;
 import com.ultimismc.skywars.core.game.features.kits.AbstractKit;
 import com.ultimismc.skywars.core.game.features.kits.KitRarity;
+import lombok.Getter;
 import org.bukkit.Material;
 
 /**
  * @author DirectPlan
  */
+@Getter
 public class EngineerKit extends AbstractKit {
 
+    private final PurchasableDesign design = new PurchasableDesign(Material.REDSTONE);
+
     public EngineerKit() {
-        super(Material.REDSTONE, "Engineer", KitRarity.RARE);
+        super("Engineer", KitRarity.RARE);
     }
 }
