4486ed27190d8d69f1071d0bc19e294b831dbe92
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-26 03:20:27 +0100

    cages and kill messages categories were built

--- lobby/src/main/java/com/ultimismc/skywars/lobby/shop/cosmetics/CosmeticsProductCategoryBuilder.java
+++ lobby/src/main/java/com/ultimismc/skywars/lobby/shop/cosmetics/CosmeticsProductCategoryBuilder.java
@@ -1,26 +1,29 @@
 package com.ultimismc.skywars.lobby.shop.cosmetics;
 
 import com.ultimismc.skywars.core.game.features.FeatureHandler;
+import com.ultimismc.skywars.core.game.features.cosmetics.CosmeticManager;
 import com.ultimismc.skywars.core.user.User;
 import com.ultimismc.skywars.lobby.shop.UserProductCategoryBuilder;
 import com.ultimismc.skywars.lobby.shop.cosmetics.ballons.BallonsCategory;
-import com.ultimismc.skywars.lobby.shop.cosmetics.cages.CagesCategory;
+import com.ultimismc.skywars.lobby.shop.cosmetics.cages.CagesProductCategoryBuilder;
 import com.ultimismc.skywars.lobby.shop.cosmetics.deathcries.DeathCriesCategory;
 import com.ultimismc.skywars.lobby.shop.cosmetics.killeffects.KillEffectsCategory;
-import com.ultimismc.skywars.lobby.shop.cosmetics.killmessages.KillMessagesCategory;
+import com.ultimismc.skywars.lobby.shop.cosmetics.killmessages.KillMessagesCategoryBuilder;
 import com.ultimismc.skywars.lobby.shop.cosmetics.sprays.SpraysCategory;
 import com.ultimismc.skywars.lobby.shop.cosmetics.trails.ProjectileTrailsCategory;
 import com.ultimismc.skywars.lobby.shop.cosmetics.victorydances.VictoryDancesCategory;
-import lombok.RequiredArgsConstructor;
 import xyz.directplan.directlib.shop.ProductCategory;
 
 /**
  * @author DirectPlan
  */
-@RequiredArgsConstructor
 public class CosmeticsProductCategoryBuilder implements UserProductCategoryBuilder {
 
-    private final FeatureHandler featureHandler;
+    private final CosmeticManager cosmeticManager;
+
+    public CosmeticsProductCategoryBuilder(FeatureHandler featureHandler) {
+        cosmeticManager = featureHandler.getCosmeticManager();
+    }
 
     @Override
     public ProductCategory<User> buildCategory() {
@@ -28,12 +31,12 @@ public class CosmeticsProductCategoryBuilder implements UserProductCategoryBuild
         CosmeticProductCategory productCategory = new CosmeticProductCategory(14);
 
         productCategory.addProduct(new ProjectileTrailsCategory(10));
-        productCategory.addProduct(new CagesCategory(12));
+        productCategory.buildProductCategory(new CagesProductCategoryBuilder(cosmeticManager));
         productCategory.addProduct(new VictoryDancesCategory(14));
         productCategory.addProduct(new KillEffectsCategory(16));
         productCategory.addProduct(new DeathCriesCategory(28));
         productCategory.addProduct(new BallonsCategory(30));
-        productCategory.addProduct(new KillMessagesCategory(32));
+        productCategory.buildProductCategory(new KillMessagesCategoryBuilder(cosmeticManager));
         productCategory.addProduct(new SpraysCategory(34));
         return productCategory;
     }
