6bf4d42bca70bbdba2b41f0d47f6ad902598aea9
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-26 02:24:57 +0100

    PurchasableDesign will now handle the design item of a purchasable

--- core/src/main/java/com/ultimismc/skywars/core/game/features/PurchasableDesign.java
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/PurchasableDesign.java
@@ -1,14 +1,27 @@
 package com.ultimismc.skywars.core.game.features;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.bukkit.Material;
 
 /**
  * @author DirectPlan
  */
-@Data
+@RequiredArgsConstructor
+@Getter
 public class PurchasableDesign {
 
     private final Material material;
     private final int durability;
+
+    @Setter private String texture;
+
+    public PurchasableDesign(Material material) {
+        this(material, 0);
+    }
+
+    public PurchasableDesign(String texture) {
+        this(Material.SKULL_ITEM, 3);
+    }
 }
