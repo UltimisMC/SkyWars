8aa54fc574afeb094b76c49f1d581f1d8f19ca10
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-26 03:27:32 +0100

    Kill message trigger will now also send a screen title to the dead player

--- core/src/main/java/com/ultimismc/skywars/core/game/features/cosmetics/killmessages/KillMessage.java
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/cosmetics/killmessages/KillMessage.java
@@ -24,10 +24,13 @@ public abstract class KillMessage extends Cosmetic {
 
     public abstract void buildBundle(KillMessageBundle messageBundle);
 
-    public void broadcastKillMessage(EntityDamageEvent.DamageCause damageCause, User user, User killer) {
+    public void triggerKillMessage(EntityDamageEvent.DamageCause damageCause, User user, User killer) {
         MessageType messageType = MessageType.translate(damageCause);
         MessageResolver messageResolver = messageBundle.getResolver(messageType);
         String killMessage = messageResolver.resolveMessage(user, killer);
         Bukkit.broadcastMessage(PluginUtility.translateMessage(killMessage));
+
+        String screenMessage = messageBundle.resolveScreenMessage(user, killer);
+        PluginUtility.sendTitle(user.getPlayer(), 0, 60, 0, screenMessage, "");
     }
 }
