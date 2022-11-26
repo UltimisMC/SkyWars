426e14840a10aee0a9dc71a4b897cc8ae5b9633f
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-26 02:19:01 +0100

    Moved fields and methods to its corresponding class

--- core/src/main/java/com/ultimismc/skywars/core/game/features/cosmetics/Cosmetic.java
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/cosmetics/Cosmetic.java
@@ -1,11 +1,38 @@
 package com.ultimismc.skywars.core.game.features.cosmetics;
 
+import com.ultimismc.skywars.core.game.currency.Currency;
 import com.ultimismc.skywars.core.game.features.Purchasable;
+import com.ultimismc.skywars.core.game.features.PurchasableRarity;
+import lombok.Getter;
 
 /**
  * @author DirectPlan
  */
-public interface Cosmetic extends Purchasable {
+@Getter
+public abstract class Cosmetic implements Purchasable {
 
+    private final Currency currency = Currency.COIN_CURRENCY;
 
+    private final String category = "Cosmetic";
+    private final String name;
+    private final CosmeticRarity cosmeticRarity;
+    private final boolean soulWell = false;
+
+    public Cosmetic(String name, CosmeticRarity rarity) {
+        this.name = name;
+        this.cosmeticRarity = rarity;
+    }
+
+    @Override
+    public int getPrice() {
+        return cosmeticRarity.getPrice();
+    }
+
+    @Override
+    public PurchasableRarity getRarity() {
+        if(cosmeticRarity != null) {
+            return cosmeticRarity.getPurchasableRarity();
+        }
+        return null;
+    }
 }
