365320c126d8503800d2ad60374a858293fe88be
AbderrahimLach$ <serversreport15@gmail.com>
2022-11-30 23:17:47 +0100

    Built implemented cosmetics

--- lobby/src/main/java/com/ultimismc/skywars/lobby/shop/cosmetics/CosmeticsProductCategoryBuilder.java
+++ lobby/src/main/java/com/ultimismc/skywars/lobby/shop/cosmetics/CosmeticsProductCategoryBuilder.java
@@ -6,11 +6,11 @@ import com.ultimismc.skywars.core.user.User;
 import com.ultimismc.skywars.lobby.shop.UserProductCategoryBuilder;
 import com.ultimismc.skywars.lobby.shop.cosmetics.ballons.BallonsCategory;
 import com.ultimismc.skywars.lobby.shop.cosmetics.cages.CagesProductCategoryBuilder;
-import com.ultimismc.skywars.lobby.shop.cosmetics.deathcries.DeathCriesCategory;
-import com.ultimismc.skywars.lobby.shop.cosmetics.killeffects.KillEffectsCategory;
+import com.ultimismc.skywars.lobby.shop.cosmetics.deathcries.DeathCriesCategoryBuilder;
+import com.ultimismc.skywars.lobby.shop.cosmetics.killeffects.KillEffectsCategoryBuilder;
 import com.ultimismc.skywars.lobby.shop.cosmetics.killmessages.KillMessagesCategoryBuilder;
 import com.ultimismc.skywars.lobby.shop.cosmetics.sprays.SpraysCategory;
-import com.ultimismc.skywars.lobby.shop.cosmetics.trails.ProjectileTrailsCategory;
+import com.ultimismc.skywars.lobby.shop.cosmetics.trails.ProjectileTrailsCategoryBuilder;
 import com.ultimismc.skywars.lobby.shop.cosmetics.victorydances.VictoryDancesCategory;
 import xyz.directplan.directlib.shop.ProductCategory;
 
@@ -30,11 +30,11 @@ public class CosmeticsProductCategoryBuilder implements UserProductCategoryBuild
 
         CosmeticProductCategory productCategory = new CosmeticProductCategory(14);
 
-        productCategory.addProduct(new ProjectileTrailsCategory(10));
+        productCategory.buildProductCategory(new ProjectileTrailsCategoryBuilder(cosmeticManager));
         productCategory.buildProductCategory(new CagesProductCategoryBuilder(cosmeticManager));
         productCategory.addProduct(new VictoryDancesCategory(14));
-        productCategory.addProduct(new KillEffectsCategory(16));
-        productCategory.addProduct(new DeathCriesCategory(28));
+        productCategory.buildProductCategory(new KillEffectsCategoryBuilder(cosmeticManager));
+        productCategory.buildProductCategory(new DeathCriesCategoryBuilder(cosmeticManager));
         productCategory.addProduct(new BallonsCategory(30));
         productCategory.buildProductCategory(new KillMessagesCategoryBuilder(cosmeticManager));
         productCategory.addProduct(new SpraysCategory(34));
