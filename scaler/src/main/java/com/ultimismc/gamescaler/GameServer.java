41146ac9b9cc697339b9266b29b34cf009315211
DirectPlan <serversreport15@gmail.com>
2022-12-22 23:44:46 +0100

    Added game server class to game-scale module

---
+++ scaler/src/main/java/com/ultimismc/gamescaler/GameServer.java
@@ -0,0 +1,11 @@
+package com.ultimismc.gamescaler;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public abstract class GameServer {
+
+    private final String name;
+}
