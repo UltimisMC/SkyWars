25e68b641db3e578566bc2a291b4c4ea71cbda5c
AbderrahimLach$ <serversreport15@gmail.com>
2022-12-24 19:07:03 +0100

    Added a wrapper method

--- core/src/main/java/com/ultimismc/skywars/core/server/SkyWarsServerManager.java
+++ core/src/main/java/com/ultimismc/skywars/core/server/SkyWarsServerManager.java
@@ -1,14 +1,21 @@
 package com.ultimismc.skywars.core.server;
 
+import com.ultimismc.gamescaler.GameServer;
 import com.ultimismc.gamescaler.ServerManager;
+import com.ultimismc.gamescaler.ServerPlugin;
 import com.ultimismc.skywars.core.SkyWarsPlugin;
 
 /**
  * @author DirectPlan
  */
-public class SkyWarsServerManager extends ServerManager {
+public class SkyWarsServerManager extends ServerManager<SkyWarsGameServer> {
 
     public SkyWarsServerManager(SkyWarsPlugin plugin) {
-        super(new SkyWarsPluginWrapper(plugin));
+        super(new SkyWarsPluginWrapper(plugin), SkyWarsGameServer.class);
+    }
+
+    @Override
+    public SkyWarsGameServer wrap(ServerPlugin plugin, String displayName, String id, boolean lobby) {
+        return new SkyWarsGameServer(plugin, displayName, id, lobby);
     }
 }
