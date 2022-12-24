b3d0cd4960c519e96d90c8f4bb16e434a06fc83f
AbderrahimLach$ <serversreport15@gmail.com>
2022-12-24 19:06:46 +0100

    Server will now broadcast the game server once started. Subscribed to SERVER UPDATE channel

--- scaler/src/main/java/com/ultimismc/gamescaler/ServerManager.java
+++ scaler/src/main/java/com/ultimismc/gamescaler/ServerManager.java
@@ -1,5 +1,7 @@
 package com.ultimismc.gamescaler;
 
+import com.google.gson.JsonElement;
+import com.ultimismc.gamescaler.communication.ServerChannel;
 import com.ultimismc.gamescaler.communication.listener.ChannelUpdateListener;
 import com.ultimismc.gamescaler.communication.ConnectionData;
 import com.ultimismc.gamescaler.communication.JedisConnection;
@@ -14,23 +16,26 @@ import java.util.Locale;
 import java.util.Map;
 
 @Getter
-public class ServerManager {
+public abstract class ServerManager<G extends GameServer> {
 
-    private final Map<String, GameServer> gameServers = new HashMap<>();
+    private final Map<String, G> gameServers = new HashMap<>();
 
     private final ServerPlugin plugin;
 
     private final JedisConnection connection;
     private final Serializer serializer;
 
-    public ServerManager(ServerPlugin plugin, Serializer serializer) {
+    private final Class<G> gameClazz;
+
+    public ServerManager(ServerPlugin plugin, Serializer serializer, Class<G> gameClazz) {
         this.plugin = plugin;
         this.serializer = serializer;
+        this.gameClazz = gameClazz;
         this.connection = new JedisConnection();
     }
 
-    public ServerManager(ServerPlugin plugin) {
-        this(plugin, new GsonSerializer());
+    public ServerManager(ServerPlugin plugin, Class<G> gameClazz) {
+        this(plugin, new GsonSerializer(), gameClazz);
     }
 
 
@@ -44,13 +49,21 @@ public class ServerManager {
         }
         plugin.log("Connection with Redis server has been established!");
         plugin.log("Subscribing to server channels...");
-        connection.subscribe(ServerChannelConstants.SERVER_UPDATE, new ChannelUpdateListener(this));
+        connection.subscribe(ServerChannelConstants.SERVER_UPDATE, new ChannelUpdateListener<>(this));
 
 
         plugin.log("Broadcasting server data...");
 
         GameServer gameServer = new GameServer(plugin, displayName, id, lobby);
+        sendRequest(ServerChannelConstants.SERVER_UPDATE, gameServer);
+        // If everything is working correctly. The server should be registered from SERVER_UPDATE listener!
+    }
 
+    public abstract G wrap(ServerPlugin plugin, String displayName, String id, boolean lobby);
+
+    public <T> void sendRequest(ServerChannel channel, T message) {
+        JsonElement jsonMessage = serializer.serialize(message);
+        connection.sendRequest(channel, jsonMessage.toString());
     }
 
     public void connect(ConnectionData connectionData, String displayName, String id) {
@@ -59,10 +72,10 @@ public class ServerManager {
 
     public void registerGameServer(GameServer gameServer) {
         String id = gameServer.getId();
-        gameServers.put(id.toLowerCase(Locale.ROOT), gameServer);
+        gameServers.put(id.toLowerCase(Locale.ROOT), gameClazz.cast(gameServer));
     }
 
-    public Collection<GameServer> getServers() {
+    public Collection<G> getServers() {
         return gameServers.values();
     }
 }
