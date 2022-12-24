3c5562c5e9e595999b1f0c8299819bea88d3d656
AbderrahimLach$ <serversreport15@gmail.com>
2022-12-24 19:04:49 +0100

    Added some messages for server info Added generics for design purposes

--- scaler/src/main/java/com/ultimismc/gamescaler/communication/listener/ChannelUpdateListener.java
+++ scaler/src/main/java/com/ultimismc/gamescaler/communication/listener/ChannelUpdateListener.java
@@ -2,19 +2,29 @@ package com.ultimismc.gamescaler.communication.listener;
 
 import com.ultimismc.gamescaler.GameServer;
 import com.ultimismc.gamescaler.ServerManager;
+import com.ultimismc.gamescaler.communication.ServerChannelConstants;
 import com.ultimismc.gamescaler.serializer.Serializer;
 
 /**
  * @author DirectPlan
  */
-public class ChannelUpdateListener extends ServerListener<GameServer> {
+public class ChannelUpdateListener<T extends GameServer> extends ServerListener<T> {
 
-    public ChannelUpdateListener(ServerManager serverManager) {
-        super(serverManager, GameServer.class);
+    public ChannelUpdateListener(ServerManager<T> serverManager) {
+        super(serverManager, ServerChannelConstants.SERVER_UPDATE, serverManager.getGameClazz());
     }
 
     @Override
-    public void onMessageReceived(GameServer server, Serializer serializer) {
+    public void onMessageReceived(T server, Serializer serializer) {
 
+        log("Some server data has been received:");
+        log(" - ID: " + server.getId());
+        log(" - Display Name: " + server.getDisplayName());
+        log(" - Players: " + server.getOnlinePlayers() + "/" + server.getMaximumPlayers());
+        log(" - A lobby: " + server.isLobby());
+        log(" - Whitelisted: " + server.isWhitelisted());
+        log(" - State: " + (server.isLobby() ? "N/A" : server.getState()));
+
+        serverManager.registerGameServer(server);
     }
 }
