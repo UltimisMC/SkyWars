7677914fc37ede1057ecd8bf9e8a3e4462805c3b
AbderrahimLach$ <serversreport15@gmail.com>
2022-12-24 19:06:00 +0100

    Added log messages and bug fixes

--- scaler/src/main/java/com/ultimismc/gamescaler/communication/listener/ServerListener.java
+++ scaler/src/main/java/com/ultimismc/gamescaler/communication/listener/ServerListener.java
@@ -1,25 +1,45 @@
 package com.ultimismc.gamescaler.communication.listener;
 
 import com.ultimismc.gamescaler.ServerManager;
+import com.ultimismc.gamescaler.ServerPlugin;
+import com.ultimismc.gamescaler.communication.ServerChannel;
 import com.ultimismc.gamescaler.serializer.Serializer;
-import lombok.RequiredArgsConstructor;
 import redis.clients.jedis.JedisPubSub;
+
+import java.io.Serializable;
 
 /**
  * @author DirectPlan
  */
-@RequiredArgsConstructor
 public abstract class ServerListener<T> extends JedisPubSub {
 
-    protected final ServerManager serverManager;
+    protected final ServerPlugin plugin;
+
+    protected final ServerManager<? extends Serializable> serverManager;
+    private final ServerChannel channel;
     private final Class<T> castClass;
+
+    public ServerListener(ServerManager<?> serverManager, ServerChannel channel, Class<T> castClass) {
+        this.serverManager = serverManager;
+        this.channel = channel;
+        this.castClass = castClass;
+        plugin = serverManager.getPlugin();
+    }
 
     @Override
     public void onMessage(String unused, String json) {
         Serializer serializer = serverManager.getSerializer();
         T message = serializer.deserialize(json, castClass);
+        log("Received a json message: " + json);
+
         onMessageReceived(message, serializer);
     }
 
     public abstract void onMessageReceived(T message, Serializer serializer);
+
+    public void log(String message) {
+        String channelName = channel.getName();
+
+        plugin.log("[LISTENER: " + channelName + "] " + message);
+    }
 }
