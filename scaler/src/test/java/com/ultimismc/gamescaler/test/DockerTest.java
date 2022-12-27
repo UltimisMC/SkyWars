4b7cda69d3709b7973fed7fe6c9a4824a05a6e4b
AkramLZ <akramlouze2005@gmail.com>
2022-12-27 10:34:40 +0100

    Added test metadata

--- scaler/src/test/java/com/ultimismc/gamescaler/test/DockerTest.java
+++ scaler/src/test/java/com/ultimismc/gamescaler/test/DockerTest.java
@@ -7,12 +7,14 @@ import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.core.DockerClientImpl;
 import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 import com.github.dockerjava.transport.DockerHttpClient;
+import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
 public class DockerTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void testContainers() {
         DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
         DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
                 .dockerHost(config.getDockerHost())
@@ -23,7 +25,7 @@ public class DockerTest {
                 .build();
         DockerClient dockerClient = DockerClientImpl.getInstance(config, httpClient);
         for (Container container : dockerClient.listContainersCmd().exec()) {
-            System.out.println(container.getId());
+            System.out.println(container.getImage());
         }
     }
 
