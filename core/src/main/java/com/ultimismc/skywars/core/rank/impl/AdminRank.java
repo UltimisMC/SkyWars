f2e9dd571880060406baad9ae33871984c1d01ce
AbderrahimLach$ <serversreport15@gmail.com>
2023-01-08 17:40:18 +0100

    Added admin rank

---
+++ core/src/main/java/com/ultimismc/skywars/core/rank/impl/AdminRank.java
@@ -0,0 +1,13 @@
+package com.ultimismc.skywars.core.rank.impl;
+
+import com.ultimismc.skywars.core.rank.Rank;
+
+/**
+ * @author DirectPlan
+ */
+public class AdminRank extends Rank {
+
+    public AdminRank() {
+        super("Admin", "&c");
+    }
+}
