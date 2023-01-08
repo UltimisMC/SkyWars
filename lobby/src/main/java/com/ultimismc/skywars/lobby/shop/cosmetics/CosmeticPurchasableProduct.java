5192e94333b1b1d0c2e97e310836330a765fe675
AbderrahimLach$ <serversreport15@gmail.com>
2023-01-08 17:44:54 +0100

    Added cosmetic selection Added registry parameter to get the setting key for each cosmetic registry

--- lobby/src/main/java/com/ultimismc/skywars/lobby/shop/cosmetics/CosmeticPurchasableProduct.java
+++ lobby/src/main/java/com/ultimismc/skywars/lobby/shop/cosmetics/CosmeticPurchasableProduct.java
@@ -1,11 +1,15 @@
 package com.ultimismc.skywars.lobby.shop.cosmetics;
 
+import com.ultimismc.skywars.core.game.GameType;
 import com.ultimismc.skywars.core.game.features.PurchasableDesign;
 import com.ultimismc.skywars.core.game.features.PurchasableRarity;
+import com.ultimismc.skywars.core.game.features.PurchasableRegistry;
 import com.ultimismc.skywars.core.game.features.cosmetics.Cosmetic;
 import com.ultimismc.skywars.core.user.User;
 import com.ultimismc.skywars.lobby.shop.UserPurchasableProduct;
+import org.bukkit.Sound;
 import org.bukkit.event.inventory.ClickType;
+import xyz.directplan.directlib.PluginUtility;
 import xyz.directplan.directlib.shop.ProductItemDesign;
 
 import java.util.ArrayList;
@@ -17,17 +21,19 @@ import java.util.Locale;
  */
 public class CosmeticPurchasableProduct extends UserPurchasableProduct {
 
+    protected final PurchasableRegistry<?> registry;
     protected final Cosmetic cosmetic;
     protected final boolean rightclickable;
 
-    public CosmeticPurchasableProduct(Cosmetic cosmetic, boolean rightclickable) {
+    public CosmeticPurchasableProduct(PurchasableRegistry<?> registry, Cosmetic cosmetic, boolean rightclickable) {
         super(0, cosmetic);
+        this.registry = registry;
         this.cosmetic = cosmetic;
         this.rightclickable = rightclickable;
     }
 
-    public CosmeticPurchasableProduct(Cosmetic cosmetic) {
-        this(cosmetic, true);
+    public CosmeticPurchasableProduct(PurchasableRegistry<?> registry, Cosmetic cosmetic) {
+        this(registry, cosmetic, true);
     }
 
     @Override
@@ -64,8 +70,14 @@ public class CosmeticPurchasableProduct extends UserPurchasableProduct {
     }
 
     @Override
-    public void executePurchasableProduct(User user, ClickType clickType) {
-        // cosmetic selection code here
+    public void executePurchasableProduct(User user, GameType gameType, ClickType clickType) {
+        user.setSetting(registry.getSettingKey(), cosmetic);
+        PluginUtility.playSound(user.getPlayer(), Sound.CLICK);
+    }
+
+    @Override
+    public boolean isRefreshInventoryEnabled() {
+        return true;
     }
 
     @Override
