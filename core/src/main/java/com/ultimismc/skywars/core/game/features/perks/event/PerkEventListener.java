6254708cd3edcc3ddc8420c88b2daf8dbbe82b89
AbderrahimLach$ <serversreport15@gmail.com>
2023-01-08 21:01:05 +0100

    Listens for all UserSkyWarsEvent super classes

---
+++ core/src/main/java/com/ultimismc/skywars/core/game/features/perks/event/PerkEventListener.java
@@ -0,0 +1,23 @@
+package com.ultimismc.skywars.core.game.features.perks.event;
+
+import com.ultimismc.skywars.core.events.UserSkyWarsEvent;
+import com.ultimismc.skywars.core.user.User;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+/**
+ * @author DirectPlan
+ */
+@RequiredArgsConstructor
+public class PerkEventListener implements Listener {
+
+    private final PerkEventHandler eventHandler;
+
+    @EventHandler
+    public void onSkyWarsEvent(UserSkyWarsEvent event) {
+        User user = event.getUser();
+
+        eventHandler.triggerPerk(user, event);
+    }
+}
