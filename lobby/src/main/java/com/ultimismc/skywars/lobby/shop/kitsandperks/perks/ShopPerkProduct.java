12fe2bd722a243d96d3b86a49a3188a6b564ec4e
AbderrahimLach$ <serversreport15@gmail.com>
2023-01-08 17:46:26 +0100

    Added game type parameter so that each game (e.g: Normal, Insane) will be own purchases

--- lobby/src/main/java/com/ultimismc/skywars/lobby/shop/kitsandperks/perks/ShopPerkProduct.java
+++ lobby/src/main/java/com/ultimismc/skywars/lobby/shop/kitsandperks/perks/ShopPerkProduct.java
@@ -1,5 +1,6 @@
 package com.ultimismc.skywars.lobby.shop.kitsandperks.perks;
 
+import com.ultimismc.skywars.core.game.GameType;
 import com.ultimismc.skywars.core.game.features.perks.Perk;
 import com.ultimismc.skywars.core.user.User;
 import com.ultimismc.skywars.lobby.shop.UserPurchasableProduct;
@@ -12,25 +13,31 @@ import xyz.directplan.directlib.shop.ProductItemDesign;
 public class ShopPerkProduct extends UserPurchasableProduct {
 
     private final Perk perk;
+    private final GameType gameType;
     private final PerkProduct perkProduct;
 
-    public ShopPerkProduct(int itemSlot, Perk perk) {
-        super(itemSlot, perk);
+    public ShopPerkProduct(int itemSlot, Perk perk, GameType gameType) {
+        super(itemSlot, perk, gameType);
 
         this.perk = perk;
+        this.gameType = gameType;
         perkProduct = new PerkProduct(perk);
     }
 
-    public ShopPerkProduct(Perk perk) {
-        this(0, perk);
+    public ShopPerkProduct(Perk perk, GameType gameType) {
+        this(0, perk, gameType);
+    }
+
+    public ShopPerkProduct(int itemSlot, Perk perk) {
+        this(itemSlot, perk, null);
     }
 
     @Override
     public ProductItemDesign designPurchasableProduct(User user) {
-        if(!perk.isSoulWellPerk() && user.getAsset(perk) != null) return null;
+        if(!perk.isSoulWellPerk() && user.getAsset(perk, gameType) != null) return null;
         return perkProduct.designProduct(user);
     }
 
     @Override
-    public void executePurchasableProduct(User user, ClickType clickType) {}
+    public void executePurchasableProduct(User user, GameType gameType, ClickType clickType) {}
 }
