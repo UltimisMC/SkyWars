accf3d7fe821143b1b4e32453d51b3845c49c2e1
AbderrahimLach$ <serversreport15@gmail.com>
2023-01-11 22:22:01 +0100

    After 5 minutes of the game end event being start, the server will shutdown.

--- game/src/main/java/com/ultimismc/skywars/game/events/GameEndedSkyWarsEvent.java
+++ game/src/main/java/com/ultimismc/skywars/game/events/GameEndedSkyWarsEvent.java
@@ -1,5 +1,6 @@
 package com.ultimismc.skywars.game.events;
 
+import com.ultimismc.skywars.core.SkyWarsPlugin;
 import com.ultimismc.skywars.game.handler.GameHandler;
 
 /**
@@ -8,11 +9,17 @@ import com.ultimismc.skywars.game.handler.GameHandler;
 public class GameEndedSkyWarsEvent extends AbstractSkyWarsEvent {
 
     public GameEndedSkyWarsEvent(long scheduledIn) {
-        super("Game Ended!", scheduledIn);
+        super("Game End", scheduledIn);
+    }
+
+    @Override
+    public void prepare(GameHandler gameHandler) {
+
     }
 
     @Override
     public void executeEvent(GameHandler gameHandler) {
-
+        SkyWarsPlugin plugin = gameHandler.getPlugin();
+        plugin.shutdownServer("Game Ended");
     }
 }
