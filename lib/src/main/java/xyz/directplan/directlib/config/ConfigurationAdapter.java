d8de7dcca1e9c25ec87203924649509b9492b615
DirectPlan <serversreport15@gmail.com>
2024-08-07 22:47:51 +0100

    Deleted implementation of deprecated config methods An old attempt to replace String#replace with a more efficient one designed for placeholders

--- lib/src/main/java/xyz/directplan/directlib/config/ConfigurationAdapter.java
+++ lib/src/main/java/xyz/directplan/directlib/config/ConfigurationAdapter.java
@@ -1,10 +1,5 @@
 package xyz.directplan.directlib.config;
 
-import xyz.directplan.directlib.config.replacement.ReplacementBoundary;
-
-import java.util.List;
-import java.util.Map;
-
 /**
  * @author DirectPlan
  */
@@ -41,60 +36,10 @@ public abstract class ConfigurationAdapter implements ConfigurationMemory<String
             }
             Object value = get(key);
             entry.setValue(value);
-
-            prepareEntryBoundaries(entry);
         }
         // For any new key that has been added to the config file.
         // This save method will make sure it is added to config, and prevent them from restarting the server again.
         saveConfiguration();
     }
 
-
-    // ******** DEPRECATED ************
-
-    private void prepareEntryBoundaries(ConfigEntry entry) {
-        Map<String, ReplacementBoundary> boundaries = entry.getReplacementBoundaries();
-
-        boundaries.clear();
-        String key = entry.getKey();
-        Object value = entry.getValue();
-
-        if(value instanceof String) {
-            String message = (String) value;
-            prepareBoundaryMessage(boundaries, key, message);
-        }else if(value instanceof List) {
-            List<String> messages = (List<String>) value;
-            messages.forEach(message -> prepareBoundaryMessage(boundaries, key, message));
-        }
-    }
-
-    private void prepareBoundaryMessage(Map<String, ReplacementBoundary> boundaryMap, String key, String message) {
-
-        char[] chars = message.toCharArray();
-
-        // -1 indicates that the index is not found
-        int beginIndex = -1, endIndex = -1;
-
-        for(int i = 0; i < chars.length; i++) {
-            char c = chars[i];
-            if(c != '%') continue;
-            if(beginIndex <= -1) {
-                beginIndex = i;
-                continue;
-            }
-            endIndex = i;
-            // BOUNDARY SEARCH FOUND & ENDED !!!!
-            // Filtering boundary entry and adding cache.
-
-            String entryKey = message.substring(beginIndex + 1, endIndex);
-//            System.out.println("ENTRY KEY: " + entryKey + " (" + beginIndex+ ", " + endIndex+") for: " + key);
-            ReplacementBoundary replacementBoundary = new ReplacementBoundary(key, beginIndex, endIndex);
-            boundaryMap.put(entryKey, replacementBoundary);
-
-            // Resetting for other entry keys in the same message.
-            beginIndex = -1;
-        }
-
-//        System.out.println("Map size: " + boundaryMap.size());
-    }
 }
