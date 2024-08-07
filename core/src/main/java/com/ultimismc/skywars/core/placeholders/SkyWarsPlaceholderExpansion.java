0df182ad04fce3de1ed76313e45cf2337dd4970d
DirectPlan <serversreport15@gmail.com>
2024-08-07 22:52:42 +0100

    Applied the placeholder changes to the SkyWars expansion

--- core/src/main/java/com/ultimismc/skywars/core/placeholders/SkyWarsPlaceholderExpansion.java
+++ core/src/main/java/com/ultimismc/skywars/core/placeholders/SkyWarsPlaceholderExpansion.java
@@ -3,45 +3,46 @@ package com.ultimismc.skywars.core.placeholders;
 import com.ultimismc.skywars.core.SkyWarsPlugin;
 import com.ultimismc.skywars.core.game.TeamType;
 import com.ultimismc.skywars.core.placeholders.stats.*;
-import com.ultimismc.skywars.core.user.User;
 import com.ultimismc.skywars.core.user.UserManager;
-import org.bukkit.entity.Player;
+import xyz.directplan.directlib.placeholder.AbstractPlaceholderExpansion;
+import xyz.directplan.directlib.placeholder.PlaceholderProcessorRegistry;
 
 import java.util.Locale;
 
 /**
  * @author DirectPlan
  */
-public class SkyWarsPlaceholderExpansion extends UserPlaceholderFactory {
+public class SkyWarsPlaceholderExpansion extends AbstractPlaceholderExpansion {
 
+    private final SkyWarsPlugin plugin;
     private final UserManager userManager;
 
     public SkyWarsPlaceholderExpansion(SkyWarsPlugin plugin) {
         super(plugin, "skywars");
 
+        this.plugin = plugin;
         userManager = plugin.getUserManager();
-
-        registerProcessor("player-wins", new WinProcessor());
-        registerProcessor("player-kills", new KillProcessor());
-        registerProcessor("player-winstreak", new WinstreakProcessor());
-        registerProcessor("player-bestwinstreak", new BestWinstreakProcessor());
-        registerProcessor("player-prog", new CurrentProgressProcessor());
-        registerProcessor("player-progress", new CurrentProgressProcessor());
-        registerProcessor("player-maxprogress", new RequiredProgressProcessor());
-        registerProcessor("player-level", new LevelProcessor(true));
-        registerProcessor("player-level-value", new LevelProcessor(false));
-        registerProcessor("player-chat-level", new ChatLevelProcessor());
-        registerProcessor("player-coins", new CoinProcessor());
-        registerProcessor("player-souls", new SoulProcessor());
-
-        for(TeamType teamType : TeamType.values()) {
-            String name = teamType.name().toLowerCase(Locale.ROOT);
-            registerProcessor(name + "-players", new OnlinePlayersProcessor(plugin, teamType));
-        }
     }
 
     @Override
-    public User getPlayer(Player player) {
-        return userManager.getCachedUser(player);
+    public void initialize(PlaceholderProcessorRegistry processorRegistry) {
+
+        processorRegistry.register("player-wins", new WinProcessor(userManager));
+        processorRegistry.register("player-kills", new KillProcessor(userManager));
+        processorRegistry.register("player-winstreak", new WinstreakProcessor(userManager));
+        processorRegistry.register("player-bestwinstreak", new BestWinstreakProcessor(userManager));
+        processorRegistry.register("player-prog", new CurrentProgressProcessor(userManager));
+        processorRegistry.register("player-progress", new CurrentProgressProcessor(userManager)); // Make 'name' varargs to avoid duplicated code
+        processorRegistry.register("player-maxprogress", new RequiredProgressProcessor(userManager));
+        processorRegistry.register("player-level", new LevelProcessor(userManager, true));
+        processorRegistry.register("player-level-value", new LevelProcessor(userManager, false));
+        processorRegistry.register("player-chat-level", new ChatLevelProcessor(userManager));
+        processorRegistry.register("player-coins", new CoinProcessor(userManager));
+        processorRegistry.register("player-souls", new SoulProcessor(userManager));
+
+        for(TeamType teamType : TeamType.values()) {
+            String name = teamType.name().toLowerCase(Locale.ROOT);
+            processorRegistry.register(name + "-players", new OnlinePlayersProcessor(userManager, plugin, teamType));
+        }
     }
 }
