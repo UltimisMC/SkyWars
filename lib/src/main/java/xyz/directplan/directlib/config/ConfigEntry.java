7b76d4f61cf21fdd1493c9cffecdca6021a56fb9
DirectPlan <serversreport15@gmail.com>
2024-08-14 03:53:30 +0100

    Code clean up & refactor

--- lib/src/main/java/xyz/directplan/directlib/config/ConfigEntry.java
+++ lib/src/main/java/xyz/directplan/directlib/config/ConfigEntry.java
@@ -1,19 +1,14 @@
 package xyz.directplan.directlib.config;
 
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import xyz.directplan.directlib.PluginUtility;
 import xyz.directplan.directlib.config.replacement.Replacement;
-import xyz.directplan.directlib.config.replacement.ReplacementBoundary;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author DirectPlan
@@ -28,26 +23,25 @@ public interface ConfigEntry {
 
     default List<String> getStringList(boolean colored, Replacement... replacements){
         Object value = getValue();
-        List<String> tempStringList = new ArrayList<>();
+        List<String> lines = new ArrayList<>();
         if(!(value instanceof List)) {
-            return tempStringList;
+            return lines;
         }
         List<?> list = (List<?>) value;
         for(Object obj : list) {
-            if(obj instanceof String) {
-                String v = proceedReplacements(obj.toString(), replacements);
-                if(colored) {
-                    v = ChatColor.translateAlternateColorCodes('&', v);
-                }
-                if(v.contains("\n")) {
-                    String[] lines = v.split("\n");
-                    tempStringList.addAll(Arrays.asList(lines));
-                    continue;
-                }
-                tempStringList.add(v);
+            if(!(obj instanceof String)) continue;
+            String line = runReplacements(obj.toString(), replacements);
+            if(colored) line = ChatColor.translateAlternateColorCodes('&', line);
+
+            String lineSeparator = System.lineSeparator();
+            if(line.contains(lineSeparator)) {
+                String[] separatedLines = line.split(lineSeparator);
+                lines.addAll(Arrays.asList(separatedLines));
+                continue;
             }
+            lines.add(line);
         }
-        return tempStringList;
+        return lines;
     }
 
     default List<String> getStringList(Replacement... replacements) {
@@ -56,10 +50,9 @@ public interface ConfigEntry {
 
     default void broadcastMessage(Replacement... replacements) {
         if(getValue() instanceof List) {
-            List<String> lines = getStringList(replacements);
-            for(String line : lines) {
-                Bukkit.broadcastMessage(PluginUtility.translateMessage(line));
-            }
+            getStringList(replacements).stream()
+                    .map(PluginUtility::translateMessage)
+                    .forEach(Bukkit::broadcastMessage);
             return;
         }
         Bukkit.broadcastMessage(PluginUtility.translateMessage(getStringValue(replacements)));
@@ -68,31 +61,16 @@ public interface ConfigEntry {
     default void sendMessage(CommandSender sender, Replacement... replacements) {
         Object value = getValue();
         if(value instanceof List) {
-            List<String> lines = getStringList(replacements);
-            for(String line : lines) {
-                sender.sendMessage(line);
-            }
+            getStringList(replacements).forEach(sender::sendMessage);
             return;
         }
         String message = getStringValue(replacements);
         sender.sendMessage(PluginUtility.translateMessage(message));
     }
 
-    default void sendClickableMessage(Player player, String command, Replacement... replacements) {
-        String message = getStringValue(replacements);
-
-        TextComponent component = new TextComponent(TextComponent.fromLegacyText(PluginUtility.translateMessage(message)));
-        // Add a click event to the component.
-        component.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/" + command));
-
-        // Send it!
-        player.spigot().sendMessage(component);
-    }
-
-    default String proceedReplacements(String text, Replacement... replacements) {
+    default String runReplacements(String text, Replacement... replacements) {
         for(Replacement replacement : replacements) {
             text = replacement.replace(text);
-
         }
         return text;
     }
@@ -102,7 +80,7 @@ public interface ConfigEntry {
         if (!(value instanceof String)) return null;
 
         String text = value.toString();
-        return proceedReplacements(text, replacements);
+        return runReplacements(text, replacements);
     }
 
     default int getInteger() {
